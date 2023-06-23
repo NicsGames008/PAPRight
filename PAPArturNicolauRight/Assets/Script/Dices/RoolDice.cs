9d44856de7d4944711612ed940a207afad6af242
NicsGames008 <92381770+NicsGames008@users.noreply.github.com>
2023-06-23 13:13:50 +0100

    Dices add to creat character and Inventory

---
+++ PAPArturNicolauRight/Assets/Script/Dices/RoolDice.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class RoolDice : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_Text dice;
+
+    private int sides;
+
+    public void OnMouseDown(int numSides)
+    {
+        sides = numSides;
+        StartCoroutine("RollTheDice");
+    }
+
+    private IEnumerator RollTheDice()
+    {
+        int randomDiceSide = 0;
+
+        int finalSide = 0;
+
+        for (int i = 0; i <= 20; i++)
+        {
+            randomDiceSide = Random.Range(1, sides);
+
+            dice.text = randomDiceSide.ToString();
+
+            // Pause before next itteration
+            yield return new WaitForSeconds(0.05f);
+        }
+
+        finalSide = randomDiceSide + 1;
+
+        Debug.Log(finalSide);
+
+        dice.text = finalSide.ToString();
+    }
+}
